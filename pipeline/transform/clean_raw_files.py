26f62566aee85671e93882c6b3a65f9f6e8b56de
Keen Zarate <keenzarate@gmail.com>
2022-06-07 22:17:27 +0200

    updates

--- pipeline/transform/clean_raw_files.py
+++ pipeline/transform/clean_raw_files.py
@@ -1,88 +1,12 @@
 import pandas as pd
-from sqlalchemy import null 
-
-# clean MEAN TEMPERATURE data 
-
-# read dataset 
-temp_usa = pd.read_csv("/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_USA.csv", skiprows = 1)
-temp_bra = pd.read_csv("/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_BRA.csv", skiprows = 1)
-temp_chn= pd.read_csv("/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_CHN.csv",  skiprows = 1)
-temp_ind = pd.read_csv("/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_IND.csv", skiprows = 1)
-
-# add a column country so we can have an indicator when data is stacked in the end 
-temp_usa['country'] = 'United States'
-temp_bra['country'] = 'Brazil'
-temp_chn['country'] = 'China'
-temp_ind['country'] = 'India'
-
-# update the unnamed column to "year" to make it more clear
-temp_usa.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-temp_bra.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-temp_chn.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-temp_ind.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-
-# make the data long so we can stack it later on our other datasets 
-long_usa = pd.DataFrame(pd.melt(temp_usa, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-long_bra = pd.DataFrame(pd.melt(temp_bra, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-long_chn = pd.DataFrame(pd.melt(temp_chn, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-long_ind = pd.DataFrame(pd.melt(temp_ind, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-
-# stack all of the individual country dataframes 
-stacked_mean_temp = pd.concat([long_usa, long_bra, long_chn, long_ind])
-
-# add a column measure name so we know what this data is about 
-stacked_mean_temp['measure_name'] = 'mean_temperature'
-
-# sort col so we can stack it later 
-stacked_mean_temp.sort_index(axis=1)
-
-
-# CLEAN PRECIPATION DATA
-
-# the same format as the mean temperature one 
-
-# load the dataset skip the extra headers cause we dont need it.
-precip_usa = pd.read_csv("/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_USA.csv", skiprows = 1)
-precip_bra = pd.read_csv("/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_BRA.csv", skiprows = 1)
-precip_chn= pd.read_csv("/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_CHN.csv",  skiprows = 1)
-precip_ind = pd.read_csv("/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_IND.csv", skiprows = 1)
-
-# rename the unnamed column to year so we know what it is about 
-precip_usa.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-precip_bra.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-precip_chn.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-precip_ind.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
-
-# add a column called country so we have an indicator 
-precip_usa['country'] = 'United States'
-precip_bra['country'] = 'Brazil'
-precip_chn['country'] = 'China'
-precip_ind['country'] = 'India'
-
-# make the data long so we can stack with the mean dataset 
-long_usa_precip = pd.DataFrame(pd.melt(precip_usa, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-long_bra_precip = pd.DataFrame(pd.melt(precip_bra, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-long_chn_precip = pd.DataFrame(pd.melt(precip_chn, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-long_ind_precip = pd.DataFrame(pd.melt(precip_ind, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
-
-# stack everything into one dataframe 
-stacked_precip = pd.concat([long_usa_precip, long_bra_precip, long_chn_precip, long_ind_precip])
-
-# add a column measure name 
-stacked_precip['measure_name'] = 'precipitation_celsius'
-
-# sort the column again 
-stacked_precip.sort_index(axis=1)
-
-
-# CLEAN WORLD BANK DATA 
+import boto3
 
 # set to show all the columns of the data being loaded
 pd.set_option('display.max_columns', None)
 
-# list of countries we want to subset to 
-countries = ['United States', 'India', 'China', 'Brazil']
-
+##########################################
+###  WORLD BANK DATA CLEANING FUNCTION ###
+##########################################
 
 # build small function to clean data from same source -- 
 def clean_world_bank(countries:list, data_path, measure_name):
@@ -117,46 +41,159 @@ def clean_world_bank(countries:list, data_path, measure_name):
     return final_dt
 
 
+##########################################
+###     NOAA DATA CLEANING FUNCTION    ###
+##########################################
+
+# create function to clean temperature and precipitation data 
+def clean_temp_precip(path_usa, path_bra, path_chn, path_ind, measure_name):
+    
+    # read csv files 
+    read_usa = pd.read_csv(path_usa, skiprows = 1)
+    read_bra = pd.read_csv(path_bra, skiprows = 1)
+    read_chn= pd.read_csv(path_chn,  skiprows = 1)
+    read_ind = pd.read_csv(path_ind, skiprows = 1)
+
+    # add a column country so we can have an indicator when data is stacked in the end 
+    read_usa['country'] = 'United States'
+    read_bra['country'] = 'Brazil'
+    read_chn['country'] = 'China'
+    read_ind['country'] = 'India'
+
+    # update the unnamed column to "year" to make it more clear
+    read_usa.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
+    read_bra.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
+    read_chn.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
+    read_ind.rename( columns={'Unnamed: 0' :'year'}, inplace=True )
+
+    # make the data long so we can stack it later on our other datasets 
+    long_usa = pd.DataFrame(pd.melt(read_usa, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
+    long_bra = pd.DataFrame(pd.melt(read_bra, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
+    long_chn = pd.DataFrame(pd.melt(read_chn, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
+    long_ind = pd.DataFrame(pd.melt(read_ind, id_vars = ['year', 'country'], value_name = 'measure_value', var_name = 'location'))
+
+    # stack all of the individual country dataframes 
+    stacked_data = pd.concat([long_usa, long_bra, long_chn, long_ind])
+
+    # add a column measure name so we know what this data is about 
+    stacked_data['measure_name'] = measure_name
+
+    # sort col so we can stack it later 
+    stacked_data.sort_index(axis=1)
+
+    return stacked_data
+
+#################################
+### CLEAN TEMPERATURE DATA    ###
+#################################
+
+# dataset paths
+temp_usa = "/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_USA.csv"
+temp_bra = "/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_BRA.csv"
+temp_chn = "/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_CHN.csv"
+temp_ind = "/Users/keenzarate/Documents/data/ds4a/tas_timeseries_annual_cru_1901-2020_IND.csv"
+
+# clean mean temperature data 
+clean_mean_temp = clean_temp_precip(temp_usa, temp_usa, temp_usa, temp_usa, 'mean_temperature_tons')
+
+#################################
+### CLEAN PRECIPITATION DATA ###
+#################################
+
+# load the dataset skip the extra headers cause we dont need it.
+precip_usa = "/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_USA.csv"
+precip_bra = "/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_BRA.csv"
+precip_chn = "/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_CHN.csv"
+precip_ind = "/Users/keenzarate/Documents/data/ds4a/pr_timeseries_annual_cru_1901-2020_IND.csv"
+
+# clean precipitation data
+clean_precip = clean_temp_precip(precip_usa, precip_bra, precip_chn, precip_ind, 'precipitation_celsius')
+
+
+
+#################################
+###     CLEAN CEREAL DATA     ###
+#################################
+
+# list of countries we want to subset to 
+countries = ['United States', 'India', 'China', 'Brazil']
+
 # clean cereal data
 cereal = clean_world_bank(countries, '/Users/keenzarate/Documents/data/ds4a/cereal_production_world_bank.csv', 'cereal_prod_tons')
 
 
-
-# clean CO2 emission data 
+#######################################
+###   CLEAN C02 EMISSION DATASET    ###
+#######################################
 
 # load c02 emissions data 
-# data download from https://ourworldindata.org/co2-emissions
+# data downloaded from https://ourworldindata.org/co2-emissions
 carbon_emissions =  pd.read_csv('/Users/keenzarate/Documents/data/ds4a/annual-co2-emissions-per-country.csv')
 
+# rename columns
 carbon_emissions = carbon_emissions.rename({'Entity': 'location', 'Year': 'year', 'Annual CO2 emissions': 'annual_c02_emissions'}, axis = 1)
 
+# drop column we don't need so we can stack it with the rest of the weather data 
 sub_carbon_emissions = carbon_emissions.drop(['Code'], axis = 1)
 
+# rename country column to location 
 sub_carbon_emissions['country'] = sub_carbon_emissions['location']
 
+# make the data long so we can stack later 
 long_carbon_emissions = pd.DataFrame(pd.melt(sub_carbon_emissions, id_vars = ['year', 'country', 'location'], value_name = 'measure_value', var_name = 'measure_name'))
 
 # sort the column again 
 long_carbon_emissions.sort_index(axis=1)
 
 # stack mean temperature, carbon emissions, precipitation 
-stacked_all = pd.concat([long_carbon_emissions, stacked_mean_temp, stacked_precip])
+stacked_all = pd.concat([long_carbon_emissions, clean_mean_temp, clean_precip])
 
-# make the data wide 
+# make the stacked data wide 
 stacked_wide = stacked_all.pivot(index=['location', 'year'], columns='measure_name', values='measure_value').reset_index().rename_axis(None, axis=1)
 
 
 
-
-# final data output 
+#########################
+###   FINAL DATASETS  ###
+#########################
 
 # final weather data stacked 
 final_weather_data = stacked_wide
-
 
 # final cereal data 
 final_cereal_data = cereal.pivot(index=['location', 'year'], columns='measure_name', values='measure_value').reset_index().rename_axis(None, axis=1)
 
 
+#############################
+###   WRITE DATA TO CSV  ###
+#############################
+
 final_cereal_data.to_csv('/Users/keenzarate/Documents/data/ds4a/clean_cereal_data.csv')
 final_weather_data.to_csv('/Users/keenzarate/Documents/data/ds4a/clean_weather_data.csv')
+
+# data 
+cereal_data = '/Users/keenzarate/Documents/data/ds4a/clean_cereal_data.csv'
+weather_data = '/Users/keenzarate/Documents/data/ds4a/clean_weather_data.csv'
+
+
+#############################
+###   LOAD DATA TO S3     ###
+#############################
+
+# create a client to access resources with
+s3_client = boto3.client('s3', aws_access_key_id = "null", aws_secret_access_key = "null")
+
+# upload the applications to the bucket created earlier
+# we have a try/except clause to see if it fails
+try:
+    s3_client.upload_file(cereal_data, 'null' ,"clean/clean_cereal_data.csv")
+    print('File successfully loaded')
+except Exception as e:
+    print(f'{e} Client error: file could not be uploaded to S3')
+
+# load weather data to s3
+try:
+    s3_client.upload_file(weather_data, 'null' ,"clean/clean_weather_data.csv")
+    print('File successfully loaded')
+except Exception as e:
+    print(f'{e} Client error: file could not be uploaded to S3')
